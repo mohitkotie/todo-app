355b10f0eeee1446b3a208b34b27fc67b9935ed4
mohitkotie <mohitkotie42@gmail.com>
2017-08-22 02:53:25 +0530

    first commit

--- ToDo app( core data)/ViewController.swift
+++ ToDo app( core data)/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController ,UITableViewDelegate , UITableViewDataSou
         // Do any additional setup after loading the view, typically from a nib.
         
         
-    
+    print("yes")
         
         tableView.delegate = self
         tableView.dataSource = self
